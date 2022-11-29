0a33855e9e31f45e64d821b2a4da9f38bbbcbec3
Lisa Chatellier <78473267+shinboram-90@users.noreply.github.com>
2022-11-30 00:42:07 +0100

    fontawesome added + trads, blog in progress

--- js/questions.js
+++ js/questions.js
@@ -1,41 +1,24 @@
 const questionDiv = document.getElementsByClassName("section-faq__container");
-const questions = document.querySelectorAll(".section-faq__container--question-box");
+const questions = document.querySelectorAll(".questions-toggle");
 const btnMore = document.getElementsByClassName("btn-see-more");
 const btnLess = document.getElementsByClassName("btn-see-less");
 
+// .section-faq__container--question-box
 function toggleQuestion() {
     if (btnMore[0]) {
         btnMore[0].addEventListener("click", () => {
-            // incrementCounter();
             remove();
             questionDiv[0].classList.toggle("show-questions");
             console.log(questionDiv[0]);
-            // btnMore[0].style.display = "none"
-            // btnMore[0].style.display = "none"
-
-            // if (state.count % 2 == 0) {
-            //   document.body.style.overflow = 'auto';
-            // } else {
-            //   document.body.style.overflow = 'hidden';
-            // }
         });
-    } else {
-        return;
+    // } else {
+    //     return;
     }
 }
 
 function remove() {
     btnLess[0].addEventListener("click", () => {
-        // incrementCounter();
         questionDiv[0].classList.remove("show-questions");
-        // btnMore[0].style.display = "none"
-        // btnMore[0].style.display = "none"
-
-        // if (state.count % 2 == 0) {
-        //   document.body.style.overflow = 'auto';
-        // } else {
-        //   document.body.style.overflow = 'hidden';
-        // }
     });
 }
 function toggleContent() {
@@ -43,6 +26,7 @@ function toggleContent() {
         t.addEventListener("click", (e) => {
             questions.forEach((e) => {
                 e !== t || e.classList.contains("reveal-content") ? e.classList.remove("reveal-content") : e.classList.add("reveal-content");
+                console.log(e)
             });
         });
     });
